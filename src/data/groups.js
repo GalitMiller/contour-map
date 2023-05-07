3b2dab5d4b3d45914978ecb6e6d2fe57b920616f
Jonathan Miller <jonsothervoices@gmail.com>
2023-05-07 17:29:39 -0400

    diff check this

--- src/data/groups.js
+++ src/data/groups.js
@@ -2,13 +2,17 @@
 import data from "./data.json"
 import * as d3 from "d3"
 
-// const XHIGH = 2
+const SHIGH = 1
+const XHIGH = 2
 const HIGH = 5
 const MED = 10
 const LOW = 15
 
-// const lats_xhigh = d3.range(-90, 90 + XHIGH, XHIGH)
-// const lons_xhigh = d3.range(-180, 180 + XHIGH, XHIGH)
+const lats_shigh = d3.range(-90, 90 + SHIGH)
+const lons_shigh = d3.range(-180, 180 + SHIGH)
+
+const lats_xhigh = d3.range(-90, 90 + XHIGH, XHIGH)
+const lons_xhigh = d3.range(-180, 180 + XHIGH, XHIGH)
 
 const lats_high = d3.range(-90, 90 + HIGH, HIGH)
 const lons_high = d3.range(-180, 180 + HIGH, HIGH)
@@ -19,18 +23,27 @@ const lons_med = d3.range(-180, 180 + MED, MED)
 const lats_low = d3.range(-90, 90 + LOW, LOW)
 const lons_low = d3.range(-180, 180 + LOW, LOW)
 
-// const nearestXHigh = v => Math.floor(v / XHIGH) * XHIGH
+const nearestSHigh = v => Math.round(v)
+const nearestXHigh = v => Math.floor(v / XHIGH) * XHIGH
 const nearestHigh = v => Math.floor(v / HIGH) * HIGH
 const nearestMed = v => Math.floor(v / MED) * MED
 const nearestLow = v => Math.floor(v / LOW) * LOW
 
-// const GridXHigh = lats_xhigh.reduce((a, lat) => {
-//     const r = {}
-//     lons_xhigh.forEach(lon => {
-//         r[`${lat} ${lon}`] = { lat, lon, count: 0, avg: 0, key: `${lat} ${lon}` }
-//     })
-//     return { ...a, ...r }
-// }, {})
+const GridSHigh = lats_shigh.reduce((a, lat) => {
+    const r = {}
+    lons_shigh.forEach(lon => {
+        r[`${lat} ${lon}`] = { lat, lon, count: 0, avg: 0, key: `${lat} ${lon}` }
+    })
+    return { ...a, ...r }
+}, {})
+
+const GridXHigh = lats_xhigh.reduce((a, lat) => {
+    const r = {}
+    lons_xhigh.forEach(lon => {
+        r[`${lat} ${lon}`] = { lat, lon, count: 0, avg: 0, key: `${lat} ${lon}` }
+    })
+    return { ...a, ...r }
+}, {})
 
 const GridHigh = lats_high.reduce((a, lat) => {
     const r = {}
@@ -57,19 +70,33 @@ const GridLow = lats_low.reduce((a, lat) => {
 }, {})
 
 // group data by grid
-// const GroupsXHigh = data.reduce((a, c) => {
-//     const rlat = nearestXHigh(c.Latitude)
-//     const rlon = nearestXHigh(c.Longitude)
-//     const key = `${rlat} ${rlon}`
-//     let e = a[key]
-//     if (!e) {
-//         console.log('key not found', key)
-//         return a
-//     }
-//     e.avg = e.avg * e.count/(e.count+1) + c.LimitingMag/(e.count+1)
-//     e.count += 1
-//     return { ...a, [key]: e}
-// }, GridXHigh)
+const GroupsSHigh = data.reduce((a, c) => {
+    const rlat = nearestSHigh(c.Latitude)
+    const rlon = nearestSHigh(c.Longitude)
+    const key = `${rlat} ${rlon}`
+    let e = a[key]
+    if (!e) {
+        console.log('key not found', key)
+        return a
+    }
+    e.avg = e.avg * e.count/(e.count+1) + c.LimitingMag/(e.count+1)
+    e.count += 1
+    return { ...a, [key]: e}
+}, GridSHigh)
+
+const GroupsXHigh = data.reduce((a, c) => {
+    const rlat = nearestXHigh(c.Latitude)
+    const rlon = nearestXHigh(c.Longitude)
+    const key = `${rlat} ${rlon}`
+    let e = a[key]
+    if (!e) {
+        console.log('key not found', key)
+        return a
+    }
+    e.avg = e.avg * e.count/(e.count+1) + c.LimitingMag/(e.count+1)
+    e.count += 1
+    return { ...a, [key]: e}
+}, GridXHigh)
 
 const GroupsHigh = data.reduce((a, c) => {
     const rlat = nearestHigh(c.Latitude)
@@ -113,11 +140,12 @@ const GroupsLow = data.reduce((a, c) => {
     return { ...a, [key]: e}
 }, GridLow)
 
-const ScaleGranularity = g => g > 10 ? LOW : g > 5 ? MED : HIGH // f > 2 ? HIGH : XHIGH
+const ScaleGranularity = g => g > 10 ? LOW : g > 5 ? MED : g > 2 ? HIGH : g > 1 ? XHIGH : SHIGH
 
-const StrGranularity = g => g > 10 ? 'low' : g > 5 ? 'medium' : 'high'
+const StrGranularity = g => g > 10 ? 'low' : g > 5 ? 'medium' : g > 2 ? 'high' : g > 1 ? 'extra high' : 'super high'
 export {
-    // GroupsXHigh,
+    GroupsSHigh,
+    GroupsXHigh,
     GroupsHigh,
     GroupsMed,
     GroupsLow,
